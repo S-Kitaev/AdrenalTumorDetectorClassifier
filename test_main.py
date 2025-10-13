68341724ea0719e78417bfbeed88d00a13987348
skitaev <skitaev@example.com>
2025-10-13 22:08:11 +0300

    fead: additional classification model class, tests

---
+++ test_main.py
@@ -0,0 +1,17 @@
+from nnmodel.CTModel import CTModel
+
+predict_test = CTModel()
+predict_test.load()
+predict_test.predict("./test_data/ID12_NATIVE_SE1.mp4")
+
+
+# print(predict_test.model_segmentation)
+# print(predict_test.model_classification.shape)
+print("[test] ROI детекции", predict_test.detected_roi)
+print("[test] Размерность маски", predict_test.np_mask.shape)
+print("[test] Размерность видео", predict_test.np_video.shape)
+print("[test] Размерность видео с примененной маской", predict_test.video_with_mask.shape)
+print("[test] Вероятности предсказаний", predict_test.proba)
+print("[test] Метка класса", predict_test.label)
+print("[test] Название класса", predict_test.label_name)
+
